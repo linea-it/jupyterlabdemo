d37b6e80c742b2391976cdc8d9fdef7d1f412d4e
adam <athornton@gmail.com>
2017-11-14 21:45:03 -0700

    Works, without scope.

--- jupyterhub/hublauncher.sh
+++ jupyterhub/hublauncher.sh
@@ -11,8 +11,13 @@ fi
 chmod 0600 ${HOMEDIR}/jupyterhub.sqlite
 chown -R ${USER}:${GROUP} ${HOMEDIR}
 
-cd ${HOMEDIR}
 export HUB_BIND_IP=$(/sbin/ifconfig | grep 'inet ' | awk '{print $2}' | \
 			 grep -v '127.0.0.1' | head -1)
+cd /opt/lsst/software/jupyterhub/config
+rm jupyterhub_config.py
+for i in jupyterhub_config.d/*.py ; do
+    cat $i >> jupyterhub_config.py
+done
+cd ${HOMEDIR}
 exec sudo -E -u ${USER} /usr/bin/jupyterhub --debug -f \
-  /opt/lsst/software/jupyterhub/config/jupyterhub_config.py
+  /opt/lsst/software/jupyterhub/jupyterhub_config_monolithic.py
